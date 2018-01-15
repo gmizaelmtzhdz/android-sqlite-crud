349ec5fa93926bf06ebb7666d8465681d57a1260
G. Mizael Mtz. Hdz <g.mizael@hotmail.com>
2018-01-15 00:03:37 -0600

    SQLite CRUD

---
+++ app/src/main/java/com/gmmh/androidsqlitecrud/Main.java
@@ -0,0 +1,25 @@
+package com.gmmh.androidsqlitecrud;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * JavaDoc
+ * @author      G. Mizael Mtz Hdz
+ * @version     1.0.0   (current version number of program)
+ * @since       1.0.0     (the version of the package this class was first added to)
+ */
+public class Main extends Activity {
+
+    /**
+     * Función onCreate
+     * @param  savedInstanceState
+     * @return void
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
